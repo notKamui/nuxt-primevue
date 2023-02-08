0917add03e7f0a4c3d0535e8859e7c0c07ad9cb2
notKamui <teillard.jimmy@outlook.fr>
2023-02-08 23:30:09 +0100

    Added sample on playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,19 +1,19 @@
-<script setup>
-import { ref } from 'vue'
-import Chips from 'primevue/chips'
-
-const value = ref([])
-</script>
-
 <template>
   <div>
-    Nuxt module playground!
-    <Chips v-model="value">
-      <template #chip="slotProps">
-        <div>
-          <span>{{ slotProps.value }} - (active) </span>
-        </div>
-      </template>
-    </Chips>
+    <p-button v-ripple label="Hey" icon="i-check" icon-pos="left" />
   </div>
 </template>
+
+<style>
+.i-check {
+  --icon: url(./assets/icons/check.svg);
+  -webkit-mask: var(--icon) no-repeat;
+  mask: var(--icon) no-repeat;
+  -webkit-mask-size: 100% 100%;
+  mask-size: 100% 100%;
+  background-color: currentColor;
+  color: inherit;
+  width: 1.5em;
+  height: 1.5em;
+}
+</style>
