56939d4af43993b3d3d5a165d0eb0a270914c683
notKamui <teillard.jimmy@outlook.fr>
2023-04-16 16:35:33 +0200

    migrated nuxt to 3.4.1

---
+++ playground/plugins/primevue.ts
@@ -0,0 +1,10 @@
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
+import DialogService from 'primevue/dialogservice'
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.use(ToastService)
+  vueApp.use(ConfirmationService)
+  vueApp.use(DialogService)
+})
